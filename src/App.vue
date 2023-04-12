a7029e627e5d99cc4578cd2fc1aa2dfc8fb0b98e
Stefano Biddau <stefano.biddau@si2001.it>
2023-04-13 00:44:16 +0200

    project setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <RouterView />
+</template>
+
